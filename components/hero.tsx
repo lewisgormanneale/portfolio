c698b32a97aac9d793a3b47a48c35770ef108b59
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-08-06 22:43:30 +0100

    installed three js, tested adding boxes to page

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,5 +1,30 @@
+"use client";
+
+import { Canvas } from "@react-three/fiber";
+import { Box } from "./ui/threejs/box";
+
 export default function Hero() {
   return (
-    <section className="flex flex-col min-h-screen bg-cyan-950 w-full items-center p-24"></section>
+    <section className="w-screen h-screen">
+      <Canvas
+        shadows
+        className="bg-background"
+        camera={{
+          position: [-6, 7, 7],
+        }}
+      >
+        <ambientLight intensity={Math.PI / 2} />
+        <spotLight
+          position={[10, 10, 10]}
+          angle={0.15}
+          penumbra={1}
+          decay={0}
+          intensity={Math.PI}
+        />
+        <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+        <Box position={[-1.2, 0, 0]} />
+        <Box position={[1.2, 0, 0]} />
+      </Canvas>
+    </section>
   );
 }
