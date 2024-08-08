bd8761717158880339d3555bacfa4948517837a2
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-08-08 20:26:20 +0100

    fix background color for 3d hero

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useEffect, useRef } from "react";
 import { Loader } from "./ui/threejs/loader";
 import { AsciiRenderer } from "@react-three/drei";
 import { Flow } from "./ui/threejs/flow";
@@ -10,29 +10,17 @@ export default function Hero() {
   return (
     <section className="w-full h-[calc(100vh-80px)] bg-background">
       <Canvas
-        shadows
         camera={{
           position: [0, 0, 5],
         }}
       >
         <color attach="background" args={["black"]} />
         <Suspense fallback={<Loader />}>
-          <ambientLight intensity={Math.PI / 2} />
-          <spotLight
-            position={[10, 10, 10]}
-            angle={0.15}
-            penumbra={1}
-            decay={0}
-            intensity={Math.PI}
-          />
-          <pointLight
-            position={[-10, -10, -10]}
-            decay={0}
-            intensity={Math.PI}
-          />
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[10, 15, 10]} intensity={1} />
           <Flow />
+          <AsciiRenderer fgColor="cyan" bgColor="transparent" />
         </Suspense>
-        <AsciiRenderer fgColor="white" bgColor="black" />
       </Canvas>
     </section>
   );
