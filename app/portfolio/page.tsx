d2162b5623d7d2f9a54757823713f54b9828f8be
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2024-08-08 22:54:18 +0100

    create reusable project component

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,3 +1,4 @@
+import { Project } from "@/components/project";
 import { Badge } from "@/components/ui/badge";
 import { Fragment } from "react";
 
@@ -12,23 +13,19 @@ export default function Portfolio() {
           <div className="h-72 w-72 bg-secondary"></div>
         </div>
       </section>
-      <section className="h-2/3 flex items-center justify-start gap-2 font-mono">
-        <h2 className="text-4xl [writing-mode:vertical-lr] rotate-180 font-unbounded px-4">
+      <section className="h-2/3 flex items-center gap-2 font-mono">
+        <h2 className="text-4xl [writing-mode:vertical-lr] rotate-180 px-4 font-unbounded">
           all
         </h2>
-        <ul className="h-2/3">
-          <li className="flex gap-2">
-            <a href="https://teiai-group.vercel.app/">Teiai Group</a>
-            <Badge variant="outline">
-              <a href="https://teiai-group.vercel.app/">Live</a>
-            </Badge>
-            <Badge variant="outline">
-              <a href="https://github.com/lewisgormanneale/teiai-group">
-                GitHub
-              </a>
-            </Badge>
-          </li>
-        </ul>
+        <div className="h-2/3 max-w-[800px] flex gap-2">
+          <Project
+            title="Teiai Group"
+            description="Fake company website built with Nuxt, based on the fictional (and evil) Teiai Corporation from the manga Kaiji."
+            techStack={["Vue", "Nuxt", "Tailwind CSS"]}
+            liveUrl="https://teiai-group.vercel.app/"
+            githubUrl="https://github.com/lewisgormanneale/teiai-group"
+          />
+        </div>
       </section>
     </Fragment>
   );
---
+++ components/project.tsx
@@ -0,0 +1,54 @@
+import { Badge } from "./ui/badge";
+import { Button, buttonVariants } from "./ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "./ui/card";
+export function Project({
+  title,
+  description,
+  techStack,
+  liveUrl,
+  githubUrl,
+}: {
+  title: string;
+  description: string;
+  techStack: string[];
+  liveUrl: string;
+  githubUrl: string;
+}) {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle className="mb-2">{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <ul className="flex gap-2">
+          {techStack.map((tech) => (
+            <li key={tech}>
+              <Badge variant="outline">{tech}</Badge>
+            </li>
+          ))}
+        </ul>
+      </CardContent>
+      <CardFooter>
+        <div className="flex gap-4 h-full justify-end w-full">
+          <a
+            href={githubUrl}
+            className={buttonVariants({ variant: "outline" })}
+          >
+            GitHub
+          </a>
+          <a href={liveUrl} className={buttonVariants({ variant: "default" })}>
+            Live
+          </a>
+        </div>
+      </CardFooter>
+    </Card>
+  );
+}
